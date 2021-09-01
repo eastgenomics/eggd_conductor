70ccfb4ae50362ac380c6630da8e7e819f2c4e4d
Jethro Rainford <rainford1995@gmail.com>
2021-09-01 14:44:51 +0100

    code to parse samples from samplesheet and match to assay

--- src/eggd_conductor.sh
+++ src/eggd_conductor.sh
@@ -48,7 +48,7 @@ main() {
         then
             dx download $sample_sheet
         else
-            # sample sheet not given, find in data fir
+            # sample sheet not given, find in data dir
             # need to find with dx find data and also check in tars if tarred data given
             # TODO
     fi
@@ -59,7 +59,7 @@ main() {
     # we now need to do some sample sheet validation first, then check what types of samples
     # are present to determine the workflow(s) to run
 
-    #### SAMPLE SHEET VALIDATION HERE
+    #### SAMPLE SHEET VALIDATION HERE ####
 
 
     # now we need to know what samples we have, to trigger the appropriate workflows
@@ -67,8 +67,52 @@ main() {
 
     #### DO SOME MAGIC WITH A CONFIG FILE TO SET CORRECT ASSAYS
 
+    if [ -z $assay_type ];
+    then
+        # assay type not specified, use sample names and config file
+        sample_list=$(tail -n +20 "$sample_sheet" | cut -d',' -f 1)
 
+        # build array of samples to assays
+        declare -A sample_to_assay
+
+        for name in $sample_list;
+        do
+            sample_eggd_code=$(echo $name | awk -F_ '{print $NF}')  # get eggd code from end of name for now
+            sample_assay=$(grep $sample_eggd_code $config_file | awk '{print $NF}')  # get associated assay from config
+
+            if [ -z $sample_assay ];
+            then
+                # appropriate assay not found from sample egg code
+                echo 'Assay for sample $name not found in config using the following eggd code: $sample_eggd_code'
+                echo 'Exiting now.'
+                exit 1
+            fi
+
+            # add sample name to associated assay code in array, comma separated to parse later
+            if [ -v sample_to_assay[$sample_assay] ];
+            then
+                # assay key already exists
+                sample_to_assay["$sample_assay"]="sample_to_assay[$sample_assay], $name"
+            else
+                # new assay key
+                sample_to_assay["$sample_assay"]="$name"
+            fi
+        done
+    fi
+    # we now have an array of assay codes to sample names i.e.
+    # FH: X000001_EGG3, X000002_EGG3...
+    # TSOE: X000001_EGG1, X000002_EGG1...
+
+    # access all array keys with ${!sample_to_assay[@]}
+    # access all array value with ${sample_to_assay[@]}
+    # access specific key value with ${sample_to_assay[$key]}
 
     # now the fun begins of triggering each workflow type, not sure what to do for now...
+
+    # what we need:
+    # low level configs for each assay
+    # check correct data for each sample available
+    # check if it is already demultiplexed or need to run bcl2fastq
+
 
 }
