bc86763b17afa751d246e4b195de9171ee130e9c
jethror1 <rainford1995@gmail.com>
2021-12-02 15:55:34 +0000

    add check that equal number of fastqs found

--- resources/home/dnanexus/run_workflows.py
+++ resources/home/dnanexus/run_workflows.py
@@ -232,6 +232,8 @@ def add_fastqs(input_dict, fastq_details, sample=None) -> dict:
     r1_fastqs = [x for x in sample_fastqs if 'R1_001.fastq' in x[1]]
     r2_fastqs = [x for x in sample_fastqs if 'R2_001.fastq' in x[1]]
 
+    assert len(r1_fastqs) == len(r2_fastqs), f"Mismatched number of FastQs"
+
     print(f'Found {len(r1_fastqs)} R1 fastqs and {len(r2_fastqs)} R2 fastqs')
 
     for stage, inputs in input_dict.items():
