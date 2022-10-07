05f3621d5f26d347ff2f9c86c468a113b26e2c35
Jethro Rainford <rainford1995@gmail.com>
2022-10-07 14:49:04 +0100

    add use of Jira

--- resources/home/dnanexus/run_workflows/run_workflows.py
+++ resources/home/dnanexus/run_workflows/run_workflows.py
@@ -13,13 +13,14 @@ import argparse
 from collections import defaultdict
 from xml.etree import ElementTree as ET
 import json
+import os
 import sys
 
 import dxpy as dx
 import pandas as pd
 
 from utils.dx_requests import PPRINT, DXExecute, DXManage
-from utils.utils import Slack, time_stamp
+from utils.utils import Jira, Slack, time_stamp
 
 
 def parse_sample_sheet(samplesheet) -> list:
@@ -370,6 +371,15 @@ def main():
             f"per_sample key missing from {executable} in config, check config"
             "and re-run"
         )
+    
+    # add comment to Jira ticket for run to link to this eggd_conductor job
+    Jira().add_comment(
+        run_id=args.run_id,
+        comment=(
+            "This run was processed automatically by eggd_conductor: "
+            f"{os.environ.get('conductor_job_url')}"
+        )
+    )
 
     if args.bcl2fastq_id:
         # previous bcl2fastq job specified to use fastqs from
@@ -392,6 +402,15 @@ def main():
         # demultiplex set to true in config => run bcl2fastq app
         job_id = dx_execute.demultiplex()
         fastq_details = dx_manage.get_bcl2fastq_details(job_id)
+
+        # add comment to Jira ticket for run to link demultiplexing
+        Jira().add_comment(
+            run_id=args.run_id,
+            comment=(
+                "Demultiplexing job automatically run by eggd_conductor: "
+                f"{job_id}"
+            )
+        )
     else:
         # not demultiplexing or given fastqs, exit as we aren't handling
         # this for now
@@ -492,6 +511,16 @@ def main():
         fh.write(str(total_jobs))
 
     print("Completed calling jobs")
+
+    # add comment to Jira ticket for run to link to analysis project
+    Jira().add_comment(
+        run_id=args.run_id,
+        comment=(
+            "All jobs sucessfully launched by eggd_conductor. Analysis "
+            "project: platform.dnanexus.com/projects/"
+            f"{args.dx_project_id.replace('project-', '')}/monitor/"
+        )
+    )
 
 
 if __name__ == "__main__":
