1329f59bbf6e37c416609964be45a9f8a02ae1c9
Jethro Rainford <rainford1995@gmail.com>
2022-10-13 11:44:46 +0100

    more changes to get Jira integration working

--- resources/home/dnanexus/run_workflows/utils/utils.py
+++ resources/home/dnanexus/run_workflows/utils/utils.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+from inspect import trace
 import json
 import os
 import requests
 from requests.adapters import HTTPAdapter
 from requests.auth import HTTPBasicAuth
+import traceback
 from urllib3.util import Retry
 
 
@@ -66,7 +68,8 @@ class Jira():
     run to tag analysis links to
     """
     def __init__(self) -> None:
-        self.url = os.environ.get('JIRA_URL')
+        self.queue_url = os.environ.get('JIRA_QUEUE_URL')
+        self.issue_url = os.environ.get('JIRA_ISSUE_URL')
         self.token = os.environ.get('JIRA_TOKEN')
         self.email = os.environ.get('JIRA_EMAIL')
         self.headers = {
@@ -80,6 +83,10 @@ class Jira():
     def create_session(self):
         """
         Create session adapter object to use for queries
+
+        Returns
+        -------
+        session : http session object
         """
         http = requests.Session()
         retries = Retry(total=5, backoff_factor=10, method_whitelist=['POST'])
@@ -95,23 +102,28 @@ class Jira():
         -------
         list : list of all tickets with details for given queue
         """
+        print(f"Getting all Jira tickets from endpoint: {self.queue_url}")
         start = 0
         response_data = []
 
         while True:
             response = self.http.get(
-                f"{self.url}?start={start}",
+                url=f"{self.queue_url}/issue?start={start}",
                 headers=self.headers,
                 auth=self.auth
             ).json()
+
+            print(response)
 
             if response['size'] == 0:
                 break
 
             response_data.extend(response['values'])
             start += 50
+        
+        print(f"Found {len(response_data)} tickets")
 
-        return list(set(response_data))
+        return response_data
     
 
     def get_run_ticket_id(self, run_id, tickets) -> str:
@@ -131,33 +143,60 @@ class Jira():
         str
             ticket ID
         """
-        run_ticket = [
+        print("Filtering Jira tickets for current run")
+        run_ticket = list(set([
             x['id'] for x in tickets if run_id in x['fields']['summary']
-        ]
+        ]))
+        print(f"Run ticket(s) found: {run_ticket}")
 
         if not run_ticket:
             # didn't find a ticket -> either a typo in the name or ticket
             # has not yet been raised / forgotten about, send an alert and
             # continue with things since linking to Jira is non-essential
-            message = (
+            self.send_slack_alert(
                 "Error linking analysis to Jira - no Jira ticket found for "
-                f"the current sequencing run ({run_id}). Continuing with "
+                f"the current sequencing run (`{run_id}`).\nContinuing with "
                 "analysis without linking to Jira."
             )
-            Slack().send(message=message, exit_fail=False)
+
             return None
 
         if len(run_ticket) > 1:
             # found multiple tickets, this should not happen so send us an
             # alert and don't touch the tickets
-            message = ""
-            Slack().send(message=message, exit_fail=False)
+            self.send_slack_alert(
+                f"Found more than one Jira ticket for given run (`{run_id}`)"
+                f"\n{run_ticket}"
+            )
             return None
         
         return run_ticket[0]
 
-    
-    def add_comment(self, run_id, comment):
+
+    def send_slack_alert(self, message) -> None:
+        """
+        Send alert to Slack that there was an issue with querying Jira
+
+        Parameters
+        ----------
+        message : str
+            message to send to Slack
+        """
+        if not os.path.exists('jira_alert.log'):
+            # create file to not send multiple Slack messages
+            os.mknod('jira_alert.log')
+            Slack().send(
+                message=message,
+                exit_fail=False
+            )
+        else:
+            print(
+                "Slack notification for fail with Jira already sent, "
+                f"won't send current error: {message}"
+            )
+
+
+    def add_comment(self, run_id, comment, url) -> None:
         """
         Find Jira ticket for given run ID and add internal comment
 
@@ -167,7 +206,38 @@ class Jira():
             ID of sequencing run
         comment : str
             comment to add to Jira ticket
+        url : str
+            any url to add to message after comment
         """
+        if not any(([self.queue_url, self.issue_url, self.token, self.email])):
+            # none of Jira related variables defined in config, assume we
+            # aren't wanting to use Jira and continue
+            print(
+                "No required Jira variables set in config, continuinung "
+                "without using Jira"
+            )
+            return
+
+        if not all([self.queue_url, self.issue_url, self.token, self.email]):
+            # one or more required Jira variables not set in config
+            if self.token:
+                # hide the token to not send it in the Slack message
+                self.token = f"{self.token[:4]}{'*' * (len(self.token) - 4)}"
+
+            variables = (
+                f"`JIRA_QUEUE_URL: {self.queue_url}`\n"
+                f"`JIRA_ISSUE_URL: {self.issue_url}`\n"
+                f"`JIRA_TOKEN: {self.token}`\n"
+                f"`JIRA_EMAIL: {self.email}`\n"
+
+            )
+
+            self.send_slack_alert(
+                "Unable to query Jira - one or more variables "
+                f"not defined in the config\n{variables}"
+            )
+            return
+
         try:
             # put whole thing in a try execept to not cause analysis to stop
             # if there's an issue with Jira, just send an alert to slack
@@ -175,38 +245,45 @@ class Jira():
             tickets = self.get_all_tickets()
             ticket_id = self.get_run_ticket_id(run_id=run_id, tickets=tickets)
             print(f"Found Jira ticket ID {ticket_id} for run {run_id}")
-        except Exception as error:
-            Slack().send(
-                message=(
-                    f"Error finding Jira ticket for given run ({run_id}).\n"
-                    "Continuing analysis without linking to Jira ticket."
-                )
+        except:
+            self.send_slack_alert(
+                f"Error finding Jira ticket for given run (`{run_id}`).\n"
+                f"Continuing analysis without linking to Jira ticket.\n"
+                f"Error: ```{traceback.format_exc()}```"
             )
             return
-
 
         if not ticket_id:
             # no ticket found or more than one ticket found, Slack alert
             # will have been sent in get_run_ticket_id()
             return
         
-        comment_url = (
-            "https://cuhbioinformatics.atlassian.net/rest/api/3/"
-            f"issue/{ticket_id}/comment"
-        )
+        comment_url = f"{self.issue_url}/{ticket_id}/comment"
 
         payload = json.dumps({
             "body": {
-                "type": "doc",
-                "version": 1,
-                "content": [{
-                    "type": "paragraph",
-                    "content": [{
+            "type": "doc",
+            "version": 1,
+            "content": [{
+                "type": "paragraph",
+                "content": [
+                    {
                         "text": f"{comment}",
                         "type": "text"
-                    }]
-                }]
-            },
+                    }, 
+                    {
+                        "text": f"{url}",
+                        "type": "text",
+                        "marks": [{
+                            "type": "link",
+                            "attrs": {
+                                "href": f"{url}"
+                            }
+                        }]
+                    }
+                ]
+            }]
+        },
             "properties": [{
                 "key": "sd.public.comment",
                 "value": {
@@ -216,7 +293,7 @@ class Jira():
         })
 
         response = self.http.post(
-            comment_url,
+            url=comment_url,
             data=payload,
             headers=self.headers,
             auth=self.auth
@@ -225,12 +302,11 @@ class Jira():
         if not response.status_code == 201:
             # some kind of error occurred adding Jira comment =>
             # send a non-exiting Slack alert 
-            message = (
+            self.send_slack_alert(
                 f"failed to add comment to Jira ticket ({ticket_id})\n\n"
                 f"Status code: {response.status_code}\n\n"
                 f"Error response: `{response.text}`"
             )
-            Slack().send(message=message, exit_fail=False)
 
 
 def time_stamp() -> str:
