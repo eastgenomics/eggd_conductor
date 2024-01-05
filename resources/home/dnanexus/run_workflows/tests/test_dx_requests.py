d7b4fe307f9515a42f392fba8bb5fca2c90a0bff
Jethro Rainford <rainford1995@gmail.com>
2024-01-05 10:36:32 +0000

    add tests for DXManage.get_job_output_details

--- resources/home/dnanexus/run_workflows/tests/test_dx_requests.py
+++ resources/home/dnanexus/run_workflows/tests/test_dx_requests.py
@@ -1,7 +1,11 @@
 from copy import deepcopy
 import os
-import pytest
+import unittest
+from unittest.mock import patch
 import sys
+
+import pytest
+
 sys.path.append(os.path.abspath(
     os.path.join(os.path.realpath(__file__), '../../')
 ))
@@ -53,13 +57,13 @@ class TestFilterHighestConfigVersion():
         for each assay code.
 
         Unique assay codes present between all configs:
-            EGG2, EGG5, LAB123, LAB456 
+            EGG2, EGG5, LAB123, LAB456
 
         We expect to return the following:
             - EGG2|LAB123 -> 1.2.0
             - LAB123|LAB456 -> 1.3.0
             - EGG5 -> 1.1.0
-        
+
         Rationale of each single code matching:
             - subset of unique individual codes: EGG2, EGG5, LAB123 & LAB456
             - matches for each:
@@ -197,6 +201,83 @@ class TestFilterHighestConfigVersion():
 
         with pytest.raises(AssertionError):
             DXManage.filter_highest_config_version(config_files)
+
+
+class TestDXManageGetJobOutputDetails(unittest.TestCase):
+    """
+    Tests for DXManage.get_job_output_details()
+
+    Function queries the output directory of a given job for output
+    files, then filters these down by the given job ID to ensure
+    they are output from that job. This is to get all the output
+    file details in an efficient manner since dxpy.describe on a
+    job can't return the output file details directly
+
+    Tests will just be to show that only files for the given job
+    are correctly returned
+    """
+    @patch('utils.dx_requests.dx.DXJob')
+    @patch('utils.dx_requests.dx.find_data_objects')
+    def test_only_job_specified_job_files_returned(self, mock_find, mock_job):
+        """
+        Test when a directory has other job output in that only the
+        output from the given job is returned
+        """
+        # minimal return of dx.describe on given job ID
+        mock_job.return_value.describe.return_value = {
+            'id': 'job-xxx',
+            'project': 'project-xxx',
+            'output': [
+                {
+                    'output_field1': [
+                        {'$dnanexus_link': 'file-xxx'}
+                    ]
+                }
+            ]
+        }
+
+        # minimal dx.find_data_objects return with files from multiple jobs
+        mock_find.return_value = [
+            {
+                'id': 'file-xxx',
+                'describe': {
+                    'createdBy': {
+                        'job': 'job-xxx'
+                    }
+                }
+            },
+            {
+                'id': 'file-yyy',
+                'describe': {
+                    'createdBy': {
+                        'job': 'job-yyy'
+                    }
+                }
+            },
+            {
+                'id': 'file-zzz',
+                'describe': {
+                    'createdBy': {
+                        'job': 'job-zzz'
+                    }
+                }
+            }
+        ]
+
+        files, ids = DXManage(None).get_job_output_details('job-xxx')
+
+        with self.subTest():
+            self.assertEqual(files, [mock_find.return_value[0]])
+
+        with self.subTest():
+            correct_ids = [
+                {
+                    'output_field1': [
+                        {'$dnanexus_link': 'file-xxx'}
+                    ]
+                }
+            ]
+            self.assertEqual(ids, correct_ids)
 
 
 
