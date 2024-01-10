483e8080c5de036b9df28c1f6ee5fd7f3b332c41
Jethro Rainford <rainford1995@gmail.com>
2024-01-10 10:59:20 +0000

    try fix the logging

--- resources/home/dnanexus/run_workflows/utils/dx_log.py
+++ resources/home/dnanexus/run_workflows/utils/dx_log.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 
@@ -18,8 +19,31 @@ def dx_log() -> logging.Logger:
     if os.path.exists('/opt/dnanexus/log/priority'):
         # running in DNAnexus, set up logging to monitor
         logger.addHandler(DXLogHandler())
+        logger.setFormatter(CustomLoggingFormatter())
 
     logger.propagate = False
     logger.setLevel(logging.INFO)
+    # log_format = logging.Formatter(json.dumps('$(message)s', indent='⠀⠀'))
+    # logging.getLogRecordFactory()
+
 
     return logger
+
+class CustomLoggingFormatter(logging.Formatter):
+    data = {}
+
+    def __init__(self):
+        super(CustomLoggingFormatter, self).__init__()
+
+    def format(self, record):
+        print(record)
+        record.message = record.getMessage()
+        input_data = {}
+        # input_data['@timestamp'] = datetime.utcnow().isoformat()[:-3] + 'Z'
+        input_data['level'] = record.levelname
+
+        if record.message:
+            input_data['message'] = json.dumps(record.message, indent='⠀⠀')
+
+        input_data.update(self.data)
+        return json.dumps(input_data)
