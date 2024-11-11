7ad48f650bf4875863142540f53bdf702e8c0490
Yujin Kim <yujin.kim@hotmail.fr>
2024-11-11 13:01:10 +0000

    fix for patterns

--- src/eggd_conductor.sh
+++ src/eggd_conductor.sh
@@ -328,7 +328,7 @@ main () {
     if [ "$testing_sample_limit" ]; then optional_args+="--testing_sample_limit ${testing_sample_limit} "; fi
     if [ "$mismatch_allowance" ]; then optional_args+="--mismatch_allowance ${mismatch_allowance} "; fi
     if [ "$job_reuse" ]; then optional_args+="--job_reuse ${job_reuse/ /} "; fi
-    if [ "$exclude_samples" ]; then optional_args+="--exclude_samples ${exclude_samples} "; fi
+    if [ "$exclude_samples" ]; then optional_args+="--exclude_samples '${exclude_samples}' "; fi
 
     echo "$optional_args"
 
